a0931e47cff6851054e17b2a17fe3eab2633068a
unknown <jhi002247@gmail.com>
2025-02-26 15:37:34 -0500

    fixed contact instances into experiences

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Contact from "./pages/Experiences";
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/experiences" element={<Experiences />} />
       </Routes>
     </Router>
   );
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => (
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/projects">Projects</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/experiences">Contact</Link></li>
     </ul>
   </nav>
 );

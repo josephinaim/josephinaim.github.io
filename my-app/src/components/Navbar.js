aedc49510c8eabd7662c063a2ce0e4feedeeb5c9
unknown <jhi002247@gmail.com>
2025-02-26 16:08:23 -0500

    added a navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/projects">Projects</Link></li>
-      <li><Link to="/experiences">Experiences</Link></li>
-    </ul>
-  </nav>
-);
+const NavigationBar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/experiences">Experiences</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navbar;
+export default NavigationBar;

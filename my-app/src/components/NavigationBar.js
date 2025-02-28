f610ca32428c4aa7bcdc15a9a6da88c6dd1423a4
unknown <jhi002247@gmail.com>
2025-02-28 17:27:01 -0500

    added an effect to text test

--- my-app/src/components/NavigationBar.js
+++ my-app/src/components/NavigationBar.js
@@ -5,12 +5,12 @@ const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#/">My Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#/">My Website</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#/projects">Projects</Nav.Link>
             <Nav.Link href="#/experiences">Experiences</Nav.Link>
+            <Nav.Link href="#/projects">Projects</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Home = () => (
-  <div>
-    <h1>Welcome to My Portfolio</h1>
-    <p>This is the home page.</p>
-  </div>
-);
+const Home = () => {
+  const word = "learning";
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < word.length) {
+      const timeout = setTimeout(() => {
+        setDisplayedText(word.slice(0, index + 1)); // Update text up to the current index
+        setIndex(index + 1);
+      }, 200); // Adjust speed here
+
+      return () => clearTimeout(timeout);
+    }
+  }, [index, word]); // Re-run effect when index changes
+
+  return (
+    <div>
+      <h1>Hi, I'm Ji Im.</h1>
+      <p>
+        I'm a software developer who's passionate about{" "}
+        <span style={{ fontWeight: "bold", color: "#007bff" }}>{displayedText}</span>
+        &nbsp;and growing. Click through my website to learn more about me!
+      </p>
+    </div>
+  );
+};
 
 export default Home;

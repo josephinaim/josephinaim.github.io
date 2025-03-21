405a5a664e537e35c3df00b8a9e6ca83c09ba8eb
unknown <jhi002247@gmail.com>
2025-03-21 14:28:05 -0400

    got rid of a line of space

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { motion } from "framer-motion"; 
 
-
 const Home = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
